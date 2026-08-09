cf36dd0369e4816212c7d46997e4ec89d615c564
acceptablespring1 <awsprojecttraining1@gmail.com>
2026-08-09 02:12:46 -0400

    Kubernetes addition

--- infrastructure/infrastructure/infrastructure_stack.py
+++ infrastructure/infrastructure/infrastructure_stack.py
@@ -1,15 +1,11 @@
 from aws_cdk import (
     Stack,
-    RemovalPolicy,
-    aws_s3 as s3,
     aws_ec2 as ec2,
     aws_ecr as ecr,
-    aws_ecs as ecs,
-    aws_ecs_patterns as ecs_patterns,
     aws_secretsmanager as secretsmanager,
-    aws_iam as iam
-    # Duration,
-    # aws_sqs as sqs,
+    aws_iam as iam,
+    aws_eks as eks,
+    lambda_layer_kubectl_v31 as kubectl_v31
 )
 from constructs import Construct
 
@@ -22,46 +18,17 @@ class InfrastructureStack(Stack):
             self, "ClinicalTrialSecrets",
             "arn:aws:secretsmanager:us-east-1:538565434143:secret:clinical-trial-secrets-r6F0yM"
         )
+        
         repo = ecr.Repository.from_repository_name(self, "ClinicalTrialRepo", "clinical-trial-assistant")
+        
         vpc = ec2.Vpc(self, "ClinicalTrialVpc", max_azs=2)
-        cluster = ecs.Cluster(self, "ClinicalTrialCluster", vpc=vpc)
-      
-        service = ecs_patterns.ApplicationLoadBalancedFargateService(self, "Service",
-            cluster=cluster,
-            memory_limit_mib=1024,
-            desired_count=1,
-            cpu=512,
-            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-                image=ecs.ContainerImage.from_ecr_repository(repo, 
-                tag=None),
-                container_port=8000,
-                secrets = {
-                    "OPENAI_API_KEY": ecs.Secret.from_secrets_manager(secret, "OPENAI_API_KEY"),
-                    "PINECONE_API_KEY": ecs.Secret.from_secrets_manager(secret, "PINECONE_API_KEY"),
-                    "LANGFUSE_SECRET_KEY": ecs.Secret.from_secrets_manager(secret, "LANGFUSE_SECRET_KEY"),
-                    "LANGFUSE_PUBLIC_KEY": ecs.Secret.from_secrets_manager(secret, "LANGFUSE_PUBLIC_KEY"),
-                    "LANGFUSE_BASE_URL": ecs.Secret.from_secrets_manager(secret, "LANGFUSE_BASE_URL"),
-                    "AWS_REGION_NAME": ecs.Secret.from_secrets_manager(secret, "AWS_REGION_NAME"),
-                    "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY": ecs.Secret.from_secrets_manager(secret, "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY"),
-                    "CLERK_SECRET_KEY": ecs.Secret.from_secrets_manager(secret, "CLERK_SECRET_KEY"),
-                    "CLERK_JWKS_URL": ecs.Secret.from_secrets_manager(secret, "CLERK_JWKS_URL"),
-                    "CLERK_WEBHOOK_SECRET": ecs.Secret.from_secrets_manager(secret, "CLERK_WEBHOOK_SECRET"),
-                }
+        
+        cluster = eks.Cluster(self, "ClinicalTrialCluster",
+            version=eks.KubernetesVersion.V1_31,
+            vpc=vpc,
+            default_capacity=1,
+            default_capacity_instance=ec2.InstanceType.of(
+                ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM
             ),
-            min_healthy_percent=100,
-            listener_port=80,
-
-    )
-        secret.grant_read(service.task_definition.execution_role)
-        service.target_group.configure_health_check(
-            path="/health",
-            port="8000"
+            kubectl_layer=kubectl_v31.KubectlV31Layer(self, "KubectlLayer")
         )
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "InfrastructureQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
