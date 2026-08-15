17fccce9cede3bc40797d51a344ad336d298a1fc
acceptablespring1 <awsprojecttraining1@gmail.com>
2026-08-15 09:31:45 -0400

    allowing domain again

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.include_router(webhooks.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://clinical-trial-frontend.vercel.app"
+                   "https://clinical-trial-frontend.vercel.app",
                    "https://clinical.zaysprojectsite.com"
                    ],
     allow_credentials=True,
